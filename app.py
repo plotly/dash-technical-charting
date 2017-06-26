40243ce296df944ccc5bacedc0e85927557ff0a3
Jack Luo <thejackluo@gmail.com>
2017-06-26 18:04:00 -0400

    Test

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ app.css.append_css({
     )
 })
 
-if 'DYNO' in os.environ:
-    app.scripts.append_script({
-        'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'  # noqa: E501
-    })
+# if 'DYNO' in os.environ:
+#     app.scripts.append_script({
+#         'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'  # noqa: E501
+#     })
 
 # Add caching
 cache = Cache(app.server, config={'CACHE_TYPE': 'simple'})
